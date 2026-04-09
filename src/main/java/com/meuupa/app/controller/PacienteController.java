3a91767f8ccc49fd447858bee3f2d6c3b52f5336
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-04-09 10:21:03 -0300

    Controller terminado

--- src/main/java/com/meuupa/app/controller/PacienteController.java
+++ src/main/java/com/meuupa/app/controller/PacienteController.java
@@ -1,5 +1,36 @@
 package com.meuupa.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meuupa.app.model.Paciente;
+import com.meuupa.app.service.PacienteService;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
 
+	@Autowired
+	private PacienteService pacienteService;
+	
+	@GetMapping
+	public ResponseEntity<List<Paciente>> listarTodos(){
+		 	List<Paciente> lista = pacienteService.listarTodos();
+		 	return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Paciente> salvar(@RequestBody Paciente paciente){
+		Paciente salvo =  pacienteService.salvar(paciente);
+		return ResponseEntity.ok(salvo);
+	}
+	
 }
