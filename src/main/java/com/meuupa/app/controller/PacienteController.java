c9904dd3ba4d04034593fc9496bca44f882662cd
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-04-09 17:59:01 -0300

    addicao do controler e criacao do html

--- src/main/java/com/meuupa/app/controller/PacienteController.java
+++ src/main/java/com/meuupa/app/controller/PacienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,10 +36,15 @@ public class PacienteController {
 		return ResponseEntity.ok(salvo);
 	}
 	
-	@DeleteMapping("/id")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deletar(@PathVariable Long id){
 		pacienteService.deletar(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Paciente> atualizar(@PathVariable Long id, @RequestBody Paciente paciente){
+		return ResponseEntity.ok(pacienteService.atualizar(id, paciente));
 	}
 	
 	
