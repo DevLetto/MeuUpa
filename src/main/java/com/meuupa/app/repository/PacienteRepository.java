13597b7229faabdc365ad30c91a98d21635f6e75
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-04-09 09:57:49 -0300

    Configurado o Repositório

--- src/main/java/com/meuupa/app/repository/PacienteRepository.java
+++ src/main/java/com/meuupa/app/repository/PacienteRepository.java
@@ -1,5 +1,10 @@
 package com.meuupa.app.repository;
 
-public interface PacienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.meuupa.app.model.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+ 
 }
