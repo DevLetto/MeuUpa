78c5de90dc8334ba2fe3451519c7ca9f64bb4b3c
AdlerMG <adlermoraesg@gmail.com>
2026-04-10 14:10:09 -0300

    Navbar

--- src/main/java/com/meuupa/app/controller/PacienteController.java
+++ src/main/java/com/meuupa/app/controller/PacienteController.java
@@ -4,49 +4,52 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller; // ✅ ADICIONADO
+import org.springframework.ui.Model; // ✅ ADICIONADO
+import org.springframework.web.bind.annotation.*;
 
 import com.meuupa.app.model.Paciente;
 import com.meuupa.app.service.PacienteService;
 
-@RestController
+@Controller 
 @RequestMapping("/pacientes")
 public class PacienteController {
 
 	@Autowired
 	private PacienteService pacienteService;
 	
+	
+	@GetMapping("/formulario")
+	public String formulario(Model model) {
+		model.addAttribute("paciente", new Paciente());
+		return "cadastro";
+	}
+	
+
 	@GetMapping
+	@ResponseBody
 	public ResponseEntity<List<Paciente>> listarTodos(){
-		 	List<Paciente> lista = pacienteService.listarTodos();
-		 	return ResponseEntity.ok(lista);
-		
+		List<Paciente> lista = pacienteService.listarTodos();
+		return ResponseEntity.ok(lista);
 	}
 	
 	@PostMapping
+	@ResponseBody
 	public ResponseEntity<Paciente> salvar(@RequestBody Paciente paciente){
 		Paciente salvo =  pacienteService.salvar(paciente);
 		return ResponseEntity.ok(salvo);
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseBody
 	public ResponseEntity<Void> deletar(@PathVariable Long id){
 		pacienteService.deletar(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("/{id}")
+	@ResponseBody
 	public ResponseEntity<Paciente> atualizar(@PathVariable Long id, @RequestBody Paciente paciente){
 		return ResponseEntity.ok(pacienteService.atualizar(id, paciente));
 	}
-	
-	
-	
 }
