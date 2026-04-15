9960b8f66512f0139d9de731e938d4f9cf608fb3
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-04-15 18:09:50 -0300

    Correção final MVP

--- src/main/java/com/meuupa/app/controller/PacienteController.java
+++ src/main/java/com/meuupa/app/controller/PacienteController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller; // ✅ ADICIONADO
-import org.springframework.ui.Model; // ✅ ADICIONADO
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.*;
 
 import com.meuupa.app.model.Paciente;
